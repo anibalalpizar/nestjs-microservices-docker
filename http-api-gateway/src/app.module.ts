f11f9e713c70c6fd00cc9612d361cd3f3f0bf157
anibalalpizar <anibal.alpizar14@gmail.com>
2024-09-26 21:32:48 -0600

    Refactor app.module.ts to import UsersModule for user-related functionality

--- http-api-gateway/src/app.module.ts
+++ http-api-gateway/src/app.module.ts
@@ -1,17 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ClientsModule, Transport } from '@nestjs/microservices';
+import { UsersModule } from './users/users.modules';
 
 @Module({
   imports: [
-    ClientsModule.register([
-      {
-        name: 'NATS_SERVICE',
-        transport: Transport.NATS,
-        options: {
-          servers: ['nats://nats'],
-        },
-      },
-    ]),
+    
+    UsersModule,
   ],
   controllers: [],
   providers: [],
---
+++ http-api-gateway/src/users/users.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Inject, Post } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+
+@Controller('users')
+// no comunicate with a database, communicate with a nats server
+// request body is a json object (username, email, etc)
+// send a message to the nats server
+// nats server will send a message to the users-microservice
+// extra: the users-microservice have to be subscribed to the nats server
+export class UsersController {
+  constructor(
+    @Inject('NATS_SERVICE') private readonly natsClient: ClientProxy,
+  ) {}
+
+  @Post()
+  createUser() {
+  }
+}
