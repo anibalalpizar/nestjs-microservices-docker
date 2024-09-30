fc5c73e76feffcdbfdb61f1023043d8a45c627ba
anibalalpizar <anibal.alpizar14@gmail.com>
2024-09-29 23:33:12 -0600

    Add User entity and configure TypeORM in UsersModule

--- users-microservice/src/users/users.module.ts
+++ users-microservice/src/users/users.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UserMicroserviceController } from './users.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/typeorm/entities/User';
+import { UsersService } from './users.service';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UserMicroserviceController],
-  providers: [],
+  providers: [UsersService],
 })
 export class UsersModule {}
