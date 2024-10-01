664b7c636765a78ce23e4aee47e67b6d2ea100a2
anibalalpizar <anibal.alpizar14@gmail.com>
2024-09-30 23:07:30 -0600

    Add MySQL database configuration and Payment entity

--- payments-microservice/src/app.module.ts
+++ payments-microservice/src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
 import { PaymentsModule } from './payments/payments.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Payment } from './typeorm/entities/Payment';
 
 @Module({
-  imports: [PaymentsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'mysql_db',
+      port: 3307,
+      database: 'nest_microservices',
+      entities: [Payment],
+      synchronize: true,
+      username: 'testuser',
+      password: 'testuser123',
+    }),
+    PaymentsModule,
+  ],
   controllers: [],
   providers: [],
 })
