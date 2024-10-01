626ecf1b690ad9e29a08a75868a8e8299bb894ef
anibalalpizar <anibal.alpizar14@gmail.com>
2024-10-01 02:18:29 -0600

    Add Payment entity and update UsersModule to include Payment entity

--- users-microservice/src/users/users.module.ts
+++ users-microservice/src/users/users.module.ts
@@ -3,9 +3,10 @@ import { UserMicroserviceController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/typeorm/entities/User';
 import { UsersService } from './users.service';
+import { Payment } from 'src/typeorm/entities/Payment';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, Payment])],
   controllers: [UserMicroserviceController],
   providers: [UsersService],
 })
